d477a295096fc36627aae5a8e7bafe41705e88d4
Pavel Shebeko <Publicsbk@mail.ru>
2022-12-09 11:48:03 +0300

    Change oop.Computer

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -6,14 +6,25 @@ public class Computer {
     private int ssd;
     private String cpu;
 
-    public void printInfo() {
-        System.out.println("Много мониторов: " + multiMonitor);
-        System.out.println("SSD: " + ssd + " GB");
-        System.out.println("Модель CPU: " + cpu);
-    }
-
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(int ssd, String cpu, boolean multiMonitor) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(int ssd, String cpu) {
         this.ssd = ssd;
         this.cpu = cpu;
     }
@@ -21,10 +32,18 @@ public class Computer {
     public Computer() {
     }
 
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
+    }
+
     public static void main(String[] args) {
-        Computer computer1 = new Computer();
         Computer computer = new Computer(true, 500, "I5- 5000");
-        computer1.printInfo();
+        Computer computer1 = new Computer(true, 512.0, "XXX");
+        Computer computer2 = new Computer();
         computer.printInfo();
+        computer1.printInfo();
+        computer2.printInfo();
     }
 }
