bddcf85cc82de951da64f69b914d069521cb9828
Pavel Shebeko <Publicsbk@mail.ru>
2022-12-19 14:24:48 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #371876]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,7 +58,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
-            size--;
+            items[size--] = null;
         }
         return rsl;
     }
