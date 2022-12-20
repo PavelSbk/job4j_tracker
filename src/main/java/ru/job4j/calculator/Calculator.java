9dc32be18e5fcd79865f65e639e9d11973e1e790
Pavel Shebeko <Publicsbk@mail.ru>
2022-12-20 14:12:05 +0300

    4.3. Вызов метода без указания класса [#504945]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -9,4 +9,21 @@ public class Calculator {
     public double add(double first, double second, double third) {
         return add(first, add(second, third));
     }
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
+    }
 }
