ec85cdb4d13693fa5a309610089682fef164e27f
Pavel Shebeko <Publicsbk@mail.ru>
2022-12-26 15:01:12 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #374865]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -11,61 +11,42 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!hasUpperCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-        }
-        if (!hasLowerCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-        }
-        if (!hasDigit(password)) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-        if (!hasSymbol(password)) {
-            throw new IllegalArgumentException("Password should contain at least one special symbol");
-        }
         if (hasSubstrings(password)) {
             throw new IllegalArgumentException(
                     "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
             );
         }
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasDigit = false;
+        boolean hasSymbol = false;
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                hasUpper = true;
+            }
+            if (Character.isLowerCase(ch)) {
+                hasLower = true;
+            }
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+            if (!Character.isDigit(ch) && !Character.isLetter(ch)) {
+                hasSymbol = true;
+            }
+        }
+        if (!hasLower) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!hasUpper) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!hasSymbol) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
         return password;
-    }
-
-    private static boolean hasUpperCase(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isUpperCase(word.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean hasLowerCase(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isLowerCase(word.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean hasDigit(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isDigit(word.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean hasSymbol(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if (!Character.isLetter(word.charAt(i))
-                    && !Character.isDigit(word.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private static boolean hasSubstrings(String word) {
